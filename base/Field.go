c326e412c1079144d42ac1bc69312843136c7e85
tsqqqqqq <tengshengqiang@nzee.com.cn>
2021-07-14 10:19:53 +0800

    创建字段映射结构体

---
+++ base/Field.go
@@ -0,0 +1,10 @@
+package base
+
+import "reflect"
+
+// FieldInfo 字段结构体，包含字段名称，是否为逐渐，字段对应变量值
+type FieldInfo struct {
+	FieldName  string        //字段名称
+	PrimaryKey bool          //是否为主键
+	ReValue    reflect.Value //字段对应变量值 通过反射获取，reflect.Value.Interface()
+}
